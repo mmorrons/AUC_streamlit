79ca105833149f6ade6a3f4f0ff26d60520176ef
mmorrons <158231647+mmorrons@users.noreply.github.com>
2025-03-11 11:32:54 +0100

    Update SAS_area_plots.py

--- SAS_area_plots.py
+++ SAS_area_plots.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(
-    page_title="Inertial Motion Unit Data Analysis", 
-    page_icon=":rocket:",
+    page_title="SAS - AUC", 
+    page_icon=":finger:",
     layout="wide")
 
 import pandas as pd
